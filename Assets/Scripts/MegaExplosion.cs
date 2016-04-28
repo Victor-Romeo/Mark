c57b95d6b9c770201aa9c19eb945c1909015d960
ppoplawska <patrickpoplawska@gmail.com>
2016-04-28 05:23:03 -0400

    Boom Overload Explosion System

---
+++ Assets/Scripts/MegaExplosion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MegaExplosion : MonoBehaviour {
+
+
+	public GameObject[] explosionPrefabs = new GameObject[5];
+
+	// Use this for initialization
+	void Start () {
+
+		OperationBoomBoom();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OperationBoomBoom(){
+		foreach(GameObject explosion in explosionPrefabs){
+			explosion.GetComponent<Detonator>().enabled = true;
+		}
+
+	}
+}
